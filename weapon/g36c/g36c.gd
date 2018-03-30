3877373ea6df385638dc51b32c5f9820ff1c6eaa
khairul169 <me@khairul.my.id>
2018-03-30 13:29:46 +0700

    Move all gfps-spesific assets into self directory..

---
+++ weapon/g36c/g36c.gd
@@ -0,0 +1,30 @@
+extends "res://gfps/scripts/weapon/base_weapon.gd"
+
+func _init():
+	# Weapon name
+	name = "G36C";
+	
+	# Resources
+	weapon_scene = "g36c.tscn";
+	audio_shoot = "g36c-shoot.wav";
+	
+	# Weapon stats
+	clip = 40;
+	ammo = 120;
+	
+	recoil = Vector2(1.4, 1.8);
+	spread = Vector2(1.8, 6.0);
+	move_speed = 0.96;
+	
+	firing_delay = 1.0/9.0;
+	firing_range = 100.0;
+	firing_mode = MODE_AUTO;
+	
+	reload_time = 2.4;
+	muzzle_size = 1.0;
+	
+	can_aim = true;
+	aim_fov = 40.0;
+	aim_statsmultiplier = 0.4;
+	aim_bobscale = 0.1;
+	dualrender_fov = 30.0;
